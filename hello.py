0c0857232a79951bfe916acd981f117193555a15
anne-maria <anne-maria.visuri@aalto.fi>
2016-12-14 12:00:59 +0200

    Added another function to hello.py.

--- hello.py
+++ hello.py
@@ -2,3 +2,9 @@
 def say_hello():
 	print("Good midday, world!")
 	print("Bye bye world!")
+
+def say_something(something):
+	if something:
+		print(" ".join(something))
+	else:
+		print("Hello world!")
